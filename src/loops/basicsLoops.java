36d0a709f78baee2abf117a5776f67e93ab3c102
Jayanto <jayantomahato56@gmail.com>
2025-09-23 04:57:56 +0530

    update

---
+++ src/loops/basicsLoops.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class basicsLoops {
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.println("nameste duniya");
+        }
+    }
+}
